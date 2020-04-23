5ba112919b7f4d2f04a31b71ad7b2de1fa0203e7
victor-qin <lingheng.qin@gmail.com>
2020-04-23 06:53:47 -0700

    Residue implementation using KK

--- kk_implement.cpp
+++ kk_implement.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <array>
-#include <queue>
 #include <random>
 #include <chrono>
+#include <map>
 
 using namespace std;
 using namespace std::chrono;
@@ -72,14 +72,14 @@ long deleteMax(vector<long> &heap){
   heap.pop_back();
 
   maxHeapify(heap, 0);
-  return (max);
+  return(max);
 }
 
 
-int kk(vector<long>& inst, int dim){     // inst is the matrix, dim is size of it
+int kk(vector<long>& inst){     // inst is the matrix, dim is size of it
 
   vector<long> heap;
-  for(int i = 0; i < dim; i++){
+  for(int i = 0; i < inst.size(); i++){
     insert(heap, inst[i]);
   }
 
@@ -95,12 +95,58 @@ int kk(vector<long>& inst, int dim){     // inst is the matrix, dim is size of i
   return(heap[0]);
 }
 
+// Computes residue given a number partition instance and representation.
+//inst is the elements, sol is the given solution, repr is type of representation (0 is +/- 1, 1 is prepartition)
+int resid(vector<long>& inst, vector<int>& sol, int repr) {
+  if(repr == 0){
+    long sum = 0;
+
+    for(int i = 0; i < inst.size(); i++){
+      sum += inst[i];
+    }
+    return(sum);
+  } else {
+
+    // Use a map to find the unique groups in the array
+    map<int, long> prepar;
+    for(int i = 0; i < inst.size(); i++){
+
+      map<int, long>::iterator find = prepar.find(sol[i]);
+      if(find != prepar.end()){
+        prepar[sol[i]] = prepar[sol[i]] + inst[i];
+      } else {
+        prepar.emplace(sol[i], inst[i]);
+      }
+
+    }
+
+    // Push unique values into heap
+    vector<long> out;
+    map<int, long>::iterator vals = prepar.begin();
+    while(vals != prepar.end()){
+
+      out.push_back(vals->second);
+      vals++;
+    }
+
+    for(int i = 0; i < out.size(); i++){
+      cout << out[i] << "\t" << endl;
+    }
+
+    // Run the KK algorithm
+    return(kk(out));
+  }
+
+}
+
+
+
 int main(){
 
   vector<long> inst = {10, 6, 7, 5, 8};
-  int dim = inst.size();
-
-  cout << kk(inst, dim) << endl;
+  vector<int> sol = {1, 2, 2, 4, 5};
+  cout << resid(inst, sol, 1) << endl;
+  // cout << kk(inst) << endl;
 
   return 0;
 }
